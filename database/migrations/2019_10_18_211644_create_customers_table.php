2f107ab279954e7aceee3f034ebd0b74d7d5f407
jeddtony <jedtony.jt@gmail.com>
2019-10-19 07:26:55 +0100

    Create migration for customer and vendor

--- database/migrations/2019_10_18_211644_create_customers_table.php
+++ database/migrations/2019_10_18_211644_create_customers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateContactsTable extends Migration
+class CreateCustomersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateContactsTable extends Migration
      */
     public function up()
     {
-        Schema::create('contacts', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('contact_type');
             $table->string('company_name');
             $table->string('email')->unique();
             $table->string('phone_number')->unique();
@@ -34,6 +33,6 @@ class CreateContactsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('contacts');
+        Schema::dropIfExists('customers');
     }
 }
