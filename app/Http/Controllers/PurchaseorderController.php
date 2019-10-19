342ff3d32623244f00354bcdea298ac68667afb7
jeddtony <jedtony.jt@gmail.com>
2019-10-19 07:28:11 +0100

    Add Controller for PurchaseOrder

---
+++ app/Http/Controllers/PurchaseorderController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Purchaseorder;
+use Illuminate\Http\Request;
+
+class PurchaseorderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Purchaseorder  $purchaseorder
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Purchaseorder $purchaseorder)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Purchaseorder  $purchaseorder
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Purchaseorder $purchaseorder)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Purchaseorder  $purchaseorder
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Purchaseorder $purchaseorder)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Purchaseorder  $purchaseorder
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Purchaseorder $purchaseorder)
+    {
+        //
+    }
+}
