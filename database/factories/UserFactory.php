00d781a25d0ab2d452fd2b62a6d07249819ebb6e
jeddtony <jedtony.jt@gmail.com>
2019-10-21 23:55:36 +0100

    Add factories to populate database

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,9 +1,15 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Customer;
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use SebastianBergmann\CodeCoverage\Report\Xml\Unit;
+use App\Item;
+use App\Salesorder;
+use App\Vendor;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +31,64 @@ $factory->define(User::class, function (Faker $faker) {
         'remember_token' => Str::random(10),
     ];
 });
+
+$factory->define(Item::class, function (Faker $faker)
+        {
+    return[
+        'name' => $faker->name,
+        'item_type' => function ()
+         {
+            $items = ['fragile', 'edible','breakable', 'fancy', 'hardware'];
+            return $items [rand(0, 4)]; 
+        },
+        'sku' => function(){
+            $units = ['gallons', 'dozen', 'litres', 'ounce', 'yards'];
+            return $units[rand(0, 4)];
+        },
+        'unit' => function(){
+            $units = ['gallons', 'dozen', 'litres', 'ounce', 'yard'];
+            return $units[rand(0, 4)];
+        },
+        'description' => $faker->sentence,
+    ];
+});
+
+$factory->define(Customer::class, function (Faker $faker)
+    {
+        return[
+            'name' => $faker->name,
+            'company_name' => $faker->company,
+            'email' => $faker->companyEmail,
+            'phone_number' => $faker->phoneNumber,
+            'address' => $faker->address,
+            'city' => $faker->city,
+            'state' => $faker->city,
+        ];
+}
+);
+
+$factory->define(Vendor::class, function (Faker $faker)
+{
+    return [
+        'name' => $faker->name,
+        'company_name' => $faker->company,
+        'email' => $faker->companyEmail,
+        'phone_number' => $faker->phoneNumber,
+        'address' => $faker->address,
+        'city' => $faker->city,
+        'state' => $faker->city,
+    ];
+});
+
+$factory->define(Salesorder::class, function (Faker $faker)
+{
+    return [
+        'customer_id' => Customer::all()->random()->id,
+        'sales_order' => $faker->word,
+        'reference' => $faker->word,
+        'order_date' => $faker->date,
+        'shipment_date' => $faker->date,
+        'user_id' => User::all()->random()->id,
+        'package_id'=>Package::all()->random()->id
+    ]
+})
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+       factory(App\Item::class, 20)->create();
+       factory(App\Customer::class, 10)->create();
+       factory(App\Vendor::class, 10)->create();
     }
 }
