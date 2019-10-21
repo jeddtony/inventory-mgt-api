6dc359e4fe86a744f1368367f9119e2860a0d782
jeddtony <jedtony.jt@gmail.com>
2019-10-21 23:55:12 +0100

    Add create and update feature

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
 use App\Customers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
 
 class CustomersController extends Controller
 {
@@ -36,6 +39,34 @@ class CustomersController extends Controller
     public function store(Request $request)
     {
         //
+        $validator = Validator::make($request->all(),[
+            'name'=>'required|min:3',
+            'company_name'=>'required|min:3',
+            'email'=>'required|email',
+            'address'=>'required',
+            'city'=> 'required',
+            'state'=> 'required'
+        ]);
+
+        if($validator->fails()){
+            $message = array();
+            foreach($validator->errors()->getMessages() as $item){
+                array_push($message, $item);
+            }
+          return response()->json(['error'=> 'Errors exist', 'message' => $message], 401);
+        }
+
+       $customer = Customer::create([
+            'name' => $request->name,
+            'company_name' => $request->company_name,
+            'email' => $request->email,
+            'address' => $request->address,
+            'city' => $request->city,
+            'state' => $request->state,
+            'phone_number' => $request->phone_number,
+        ]);
+
+        return response()->json(['success' => 'Customer created successful', 'data' => $customer], 201);
     }
 
     /**
@@ -58,6 +89,8 @@ class CustomersController extends Controller
     public function edit(Customers $customers)
     {
         //
+        
+
     }
 
     /**
@@ -67,9 +100,37 @@ class CustomersController extends Controller
      * @param  \App\Customers  $customers
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Customers $customers)
+    public function update(Request $request, Customer $customers)
     {
         //
+        $validator = Validator::make($request->all(),[
+            'name'=>'required|min:3',
+            'company_name'=>'required|min:3',
+            'email'=>'required|email',
+            'address'=>'required',
+            'city'=> 'required',
+            'state'=> 'required'
+        ]);
+
+        if($validator->fails()){
+            $message = array();
+            foreach($validator->errors()->getMessages() as $item){
+                array_push($message, $item);
+            }
+          return response()->json(['error'=> 'Errors exist', 'message' => $message], 401);
+        }
+
+    $customers->name = $request->name;
+    $customers->company_name = $request->company_name;
+    $customers->email = $request->email;
+    $customers->address = $request->address;
+    $customers->city = $request->city;
+    $customers->state = $request->state;
+    $customers->phone_number = $request->phone_number;
+    $customers->save();
+
+        return response()->json(['success' => 'Customer details updated successfully', 'data' => $customers], 201);
+   
     }
 
     /**
--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -36,6 +36,34 @@ class VendorController extends Controller
     public function store(Request $request)
     {
         //
+        $validator = Validator::make($request->all(),[
+            'name'=>'required|min:3',
+            'company_name'=>'required|min:3',
+            'email'=>'required|email',
+            'address'=>'required',
+            'city'=> 'required',
+            'state'=> 'required'
+        ]);
+
+        if($validator->fails()){
+            $message = array();
+            foreach($validator->errors()->getMessages() as $item){
+                array_push($message, $item);
+            }
+          return response()->json(['error'=> 'Errors exist', 'message' => $message], 401);
+        }
+
+       $vendor = Vendor::create([
+            'name' => $request->name,
+            'company_name' => $request->company_name,
+            'email' => $request->email,
+            'address' => $request->address,
+            'city' => $request->city,
+            'state' => $request->state,
+            'phone_number' => $request->phone_number,
+        ]);
+
+        return response()->json(['success' => 'Vendor created successful', 'data' => $vendor], 201);
     }
 
     /**
@@ -70,6 +98,35 @@ class VendorController extends Controller
     public function update(Request $request, Vendor $vendor)
     {
         //
+             //
+             $validator = Validator::make($request->all(),[
+                'name'=>'required|min:3',
+                'company_name'=>'required|min:3',
+                'email'=>'required|email',
+                'address'=>'required',
+                'city'=> 'required',
+                'state'=> 'required'
+            ]);
+    
+            if($validator->fails()){
+                $message = array();
+                foreach($validator->errors()->getMessages() as $item){
+                    array_push($message, $item);
+                }
+              return response()->json(['error'=> 'Errors exist', 'message' => $message], 401);
+            }
+    
+        $vendor->name = $request->name;
+        $vendor->company_name = $request->company_name;
+        $vendor->email = $request->email;
+        $vendor->address = $request->address;
+        $vendor->city = $request->city;
+        $vendor->state = $request->state;
+        $vendor->phone_number = $request->phone_number;
+        $vendor->save();
+    
+            return response()->json(['success' => 'Vendor details updated successfully', 'data' => $vendor], 201);
+       
     }
 
     /**
